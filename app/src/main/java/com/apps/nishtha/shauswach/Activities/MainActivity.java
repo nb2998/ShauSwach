b30c99415fada5b469166174dd31d68e918b8bf4
Nishtha Goel <nishthagoel26@gmail.com>
2017-10-10 17:45:31 +0530

    tutorial activity

--- app/src/main/java/com/apps/nishtha/shauswach/Activities/MainActivity.java
+++ app/src/main/java/com/apps/nishtha/shauswach/Activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.apps.nishtha.shauswach.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.apps.nishtha.shauswach.Adapter.CardAdapter;
 import com.apps.nishtha.shauswach.Card;
@@ -30,5 +33,6 @@ public class MainActivity extends AppCompatActivity {
         cardAdapter=new CardAdapter(this,cardArrayList);
         recView.setLayoutManager(new LinearLayoutManager(this));
         recView.setAdapter(cardAdapter);
+
     }
 }
